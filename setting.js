f6603475fdb7c3fe649d134e3b8747fe9a4ae74a
secondfgt21 <140176974+secondfgt21@users.noreply.github.com>
2026-03-13 00:08:09 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 const chalk = require('chalk');
 const moment = require("moment-timezone");
 
-global.PAKASIR_API_KEY = "iRYxp0CEkTey9IaP7yrGG4ygNGyeVJ37"
-global.PAKASIR_PROJECT_SLUG = "oumanlin"
+global.PAKASIR_API_KEY = "0X5Oa9sSGcpsbVwMjeqhbeOnYVDFN9iB"
+global.PAKASIR_PROJECT_SLUG = "webot"
 //Setting
-global.urlBot = 'https://t.me/ouman_bot'
-global.BOT_TOKEN = "8564970869:AAEzni0_WfxAm9FJIrv20Qjr7PPjDR-pbvk" //Create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Ouman, Bot Auto Order 24 Hours |By @oumanlin " //Your bot name
-global.OWNER_ID = "8379278966" //Your id
-global.OWNER_NAME = "Oumanlin" //Your name
-global.OWNER_NUMBER = "6285169691602" //Your Telegram number
-global.OWNER = ["https://t.me/oumanlin"] //Pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
-global.CHANNEL = "https://t.me/+CD-gQ9Hd7hM1MDY1" //Your Telegram channel 
+global.urlBot = 'https://t.me/ImpuraBot'
+global.BOT_TOKEN = "8498533605:AAGR3kYuemo4AsO9KPg_kUMJc5TdhuEP4Zg" //Create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "ImpuraBot, Bot Auto Order 24 Jam " //Your bot name
+global.OWNER_ID = "8378554356" //Your id
+global.OWNER_NAME = "Impura" //Your name
+global.OWNER_NUMBER = "6281317391284" //Your Telegram number
+global.OWNER = ["https://t.me/impuraid"] //Pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.CHANNEL = "https://t.me/aiprem_info" //Your Telegram channel 
 
 //Images
 global.thumbnail = "./options/image/thumbnail.jpg"
